3b06b49268b2a20ebeb09d3704e1474e8f339daf
Michael Yeates <michaeljyeates@gmail.com>
2021-01-29 20:32:14 +0000

    Update test config

--- config/test.js
+++ config/test.js
@@ -3,7 +3,7 @@ module.exports = {
   // waxEndpoint: 'https://testnet.waxsweden.org',
   waxChainId: 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12',
   atomicEndpoint: 'https://test.wax.api.atomicassets.io',
-  dacApiEndpoint: 'http://51.195.188.13:8383',
+  dacApiEndpoint: 'https://dao-api-test.alienworlds.io',
   collectionName: 'alien.worlds',
   daoContract: 'dac.world',
   daoTokenContract: 'token.world',
