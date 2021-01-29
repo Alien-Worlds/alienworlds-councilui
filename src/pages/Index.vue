d72ed5fc8c3e980e7f328085a11b818d6ec2cdfe
Michael Yeates <michaeljyeates@gmail.com>
2021-01-29 20:43:37 +0000

    Add index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,49 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+
+    <b-container fluid>
+      <b-row>
+        <b-col class="text-center" cols="4">
+          <router-link to="/planet/eyeke">
+            <img src="https://ipfs.io/ipfs/QmW1SeninpNQUMLstTPFTv7tMkRdBZMHBJTgf17Znr1uKK" style="max-width:100%" />
+            <h4>Eyeke</h4>
+          </router-link>
+        </b-col>
+        <b-col class="text-center" cols="4">
+          <router-link to="/planet/kavian">
+            <img src="https://ipfs.io/ipfs/QmY9n6yWcyYLKJuZ2EVnL1m7iSFovA1GCabhgDa4H1gbZN" style="max-width:100%" />
+            <h4>Kavian</h4>
+          </router-link>
+        </b-col>
+        <b-col class="text-center" cols="4">
+          <router-link to="/planet/magor">
+            <img src="https://ipfs.io/ipfs/QmTSecCGktdAt7xLZs7vqiXuxe3zMNpvsSy16LQ7ShwNN5" style="max-width:100%" />
+            <h4>Magor</h4>
+          </router-link>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col class="text-center" cols="4">
+          <router-link to="/planet/naron">
+            <img src="https://ipfs.io/ipfs/QmRNzgY1gBqAS3H7nadRNg19g6aTW8hqB2EJCMX8nXFcpx" style="max-width:100%" />
+            <h4>Naron</h4>
+          </router-link>
+        </b-col>
+        <b-col class="text-center" cols="4">
+          <router-link to="/planet/eyeke">
+            <img src="https://ipfs.io/ipfs/QmbpdwnKXR2YHkgo7vQSvrQUbMJ4QYJMiAXKs8hj26XyNW" style="max-width:100%" />
+            <h4>Neri</h4>
+          </router-link>
+        </b-col>
+        <b-col class="text-center" cols="4">
+          <router-link to="/planet/veles">
+            <img src="https://ipfs.io/ipfs/QmSVnZ6gLCGTqwWeDuGnD8XqT7iBMfnwiZhD8eNcNwvcZ4" style="max-width:100%" />
+            <h4>Veles</h4>
+          </router-link>
+        </b-col>
+      </b-row>
+    </b-container>
+
   </q-page>
 </template>
 
