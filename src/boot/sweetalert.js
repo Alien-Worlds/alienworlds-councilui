7f8c570dbb95ee0563d7620885e7e0664d3ccf62
Michael Yeates <michaeljyeates@gmail.com>
2021-01-31 14:42:26 +0000

    Add sweetalert css

--- src/boot/sweetalert.js
+++ src/boot/sweetalert.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueSweetalert2 from 'vue-sweetalert2'
 
+import 'sweetalert2/dist/sweetalert2.min.css'
+
 const options = {
   confirmButtonColor: '#41b882',
   cancelButtonColor: '#c52323'
