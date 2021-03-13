351b05216bd077bcbef0f5eaeed54c0393f5ce79
Michael Yeates <michaeljyeates@gmail.com>
2021-03-13 15:41:27 +0000

    Only show login on homepage, not details if logged in already

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,20 +3,32 @@
     <div class="logo"></div>
     <h1 class="title">Planetary Councils</h1>
     <p>Welcome Explorer - Every planet needs the best people to run it and the Alien Worlds DAO system lets you decide who has control - Maybe you should be the leader!</p>
-    <login></login>
+    <div v-if="!getAccountName.wax">
+      <a role="button" tabindex="0" href="#" target="_self" class="button" @click="login('wax', $event)">Log in</a>
+    </div>
   </div>
 </template>
 
 <script>
-import Login from 'components/Login.vue'
+import { mapGetters } from 'vuex'
 export default {
   name: 'PageIndex',
-  components: {
-    Login
-  },
   data () {
     return {
       isActive: true
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getAccountName: 'ual/getAccountName'
+    })
+  },
+  methods: {
+    async login (network, event) {
+      this.$store.dispatch('ual/renderLoginModal', network, { root: true })
+      if (event) {
+        event.preventDefault()
+      }
     }
   },
   async beforeUnmount () {
